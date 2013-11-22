ef7ebc541dd865ee68faf892c30c1b20e09c35d2
unknown <jeppen@jampe.dk>
2013-11-22 18:20:01 +0800

    inital commit for new repo

---
+++ src/OPMap.js
@@ -0,0 +1,145 @@
+#pragma strict
+
+public enum OPMapType {
+	Grid,
+	WayPoint,
+	NavMesh
+}
+
+class OPMap {
+	var nodes : OPNode[] = null;
+
+	function GetNode ( position : Vector3 ) : OPNode {
+		for ( var node : OPNode in nodes ) {
+			if ( node.position == position ) {
+				return node;
+			}
+		}
+		
+		return null;
+	}
+	
+	function ListToArray ( list : List.<OPNode> ) : OPNode[] {
+		var newArray : OPNode[] = new OPNode[list.Count];
+		
+		for ( var i : int = 0; i < list.Count; i++ ) {
+			newArray[i] = list[i];
+		}
+		
+		return newArray;
+	}
+	
+	function GetIndex ( node : OPNode ) : int {
+		return System.Array.IndexOf ( nodes, node );
+	}
+	
+	function GetNeighbors ( node : OPNode ) : List.<OPNode> {
+		return node.neighbors;
+	}
+	
+	function Reset () {
+		for ( var n : OPNode in nodes ) {
+			if ( n ) {
+				n.parent = null;
+			}
+		}
+	}
+}
+
+public class OPNavMeshMap extends OPMap {
+	function OPNavMeshMap ( navMesh : OPNavMesh ) {
+		nodes = navMesh.GetNodes ();
+	
+		MonoBehaviour.Destroy ( navMesh.gameObject );
+	}
+}
+
+public class OPWayPointMap extends OPMap {
+	function OPWayPointMap ( nodeContainers : OPWayPoint[] ) {
+		var tempList : List.<OPNode> = new List.<OPNode>();
+		
+		for ( var n : OPWayPoint in nodeContainers ) {
+			n.FindNeighbors ( nodeContainers );
+			
+			tempList.Add ( n.node );
+		}
+		
+		nodes = tempList.ToArray();
+				
+		for ( var n : OPWayPoint in GameObject.FindObjectsOfType(OPWayPoint) ) {
+			MonoBehaviour.Destroy ( n.gameObject ); 
+		}
+	}
+}
+
+public class OPGridMap extends OPMap {	
+	var spacing : float;
+	
+	function OPGridMap ( start : Vector3, size : Vector3, gridSpacing : float, layerMask : LayerMask ) {
+		var tempList : List.< OPNode > = new List.< OPNode >();
+		
+		spacing = gridSpacing;
+								
+		var x : int;
+		var y : int;
+		var z : int;
+		
+		for ( x = 0; x < size.x; x++ ) {
+			for ( z = 0; z < size.z; z++ ) {
+				var from : Vector3 = new Vector3 ( start.x + (x*spacing), start.y + (size.y*spacing), start.z + (z*spacing) );
+				var hits : RaycastHit[] = RaycastContinuous ( from, layerMask );
+				
+				for ( var h : RaycastHit in hits ) {										
+					var p : Vector3 = h.point; 
+					var m : OPNode = new OPNode ( p.x, p.y, p.z );
+					tempList.Add ( m );
+				}
+			}
+		}
+		
+		nodes = tempList.ToArray();	
+		
+		for ( var a : OPNode in tempList ) {
+			FindNeighbors ( a );
+		}
+	}
+	
+	private function RaycastContinuous ( from : Vector3, layerMask : LayerMask ) : RaycastHit[] {
+		var hits : List.< RaycastHit > = new List.< RaycastHit > ();
+		var currentHit : RaycastHit;
+		
+		if ( Physics.Raycast ( from, Vector3.down, currentHit, Mathf.Infinity, layerMask ) ) {
+			hits.Add ( currentHit );
+			
+			for ( var i : int = 0; i < 50; i++ ) {
+				if ( Physics.Raycast ( currentHit.point + Vector3.down, Vector3.down, currentHit, Mathf.Infinity, layerMask ) ) {
+					var left : boolean = Physics.Raycast ( currentHit.point, Vector3.left, spacing/2, layerMask );
+					var right : boolean = Physics.Raycast ( currentHit.point, -Vector3.left, spacing/2, layerMask );
+					var forward : boolean = Physics.Raycast ( currentHit.point, Vector3.forward, spacing/2, layerMask );
+					var back : boolean = Physics.Raycast ( currentHit.point, -Vector3.forward, spacing/2, layerMask );
+					var up : boolean = Physics.Raycast ( currentHit.point, -Vector3.down, spacing/2, layerMask );
+					
+					if ( !left && !right && !forward && !back && !up ) {
+						hits.Add ( currentHit );
+					}
+				
+				} else {
+					break;
+				
+				}
+			}
+		}
+	
+		return hits.ToArray();
+	}
+	
+	private function FindNeighbors ( thisNode : OPNode ) {
+		thisNode.neighbors.Clear ();
+		
+		for ( var thatNode : OPNode in nodes ) {			
+			if ( ( thisNode.position - thatNode.position ).sqrMagnitude <= spacing * 1.1 ) {
+				thisNode.neighbors.Add ( thatNode );
+			}
+		}
+	}
+}
---
+++ src/OPScanner.js
@@ -0,0 +1,134 @@
+#pragma strict
+
+class OPScanner extends MonoBehaviour {
+	var scanOnEnable : boolean = true;
+	var layerMask : LayerMask;
+	var gridSize : Vector3;
+	var mapType : OPMapType;
+	var map : OPMap = null;
+	var heuristic : float = 10.0;
+	var spacing : float = 1.0;
+	var bounds : Bounds;
+	
+	function GetBounds () {
+	    var bounds : Bounds = new Bounds ( Vector3.zero, Vector3.zero );
+	    var pos : Vector3 = Vector3.zero;
+	    
+	    for ( var r : Renderer in FindObjectsOfType ( Renderer ) ) {
+	    	bounds.Encapsulate ( r.bounds );
+	    }
+	    
+	    pos = bounds.min;
+	    
+	    transform.position = pos;
+		gridSize = new Vector3 ( Mathf.Round ( bounds.size.x / spacing ) + 1, Mathf.Round ( bounds.size.y / spacing ) + 1, Mathf.Round ( bounds.size.z / spacing ) + 1 );
+	}
+	
+	function SetMap ( nodes : OPNode[] ) {
+		map = new OPMap ();
+		map.nodes = nodes;
+	}
+	
+	function SetMap () {
+		if ( mapType == OPMapType.Grid ) {
+			map = new OPGridMap ( transform.position, gridSize, spacing, layerMask );
+		
+		} else if ( mapType == OPMapType.NavMesh ) {
+			var navMesh : OPNavMesh = GameObject.FindObjectOfType ( OPNavMesh );
+			
+			map = new OPNavMeshMap ( navMesh );
+		
+		} else {
+			map = new OPWayPointMap ( GameObject.FindObjectsOfType ( OPWayPoint ) );
+		
+		}
+	}
+	
+	function Init () : IEnumerator {
+		Debug.Log ( "OPScanner | Scanning for navigation nodes as " + mapType + "..." );
+		
+		var timeTaken : float = Time.time;
+		
+		yield WaitForEndOfFrame ();
+	
+		GetBounds ();
+		SetMap ();
+		
+		yield WaitForEndOfFrame ();
+		
+		timeTaken = ( Time.time - timeTaken ) * 10;
+		
+		Debug.Log ( "OPScanner | ...scan completed in " + timeTaken + " seconds" );
+	}
+	
+	function Scan () {
+		StartCoroutine ( Init () );
+	}
+	
+	function Start () {
+		if ( scanOnEnable ) {
+			Scan ();
+		}
+	}
+	
+	function FindPath ( start : Vector3, goal : Vector3 ) : List.<OPNode> {
+		var here : OPNode = GetClosestNode ( start );
+		var there : OPNode = GetClosestNode ( goal );
+		var list : List.<OPNode> = OPAStar.Search ( here, there, map, heuristic );
+	
+		map.Reset ();
+	
+		return list;
+	}
+	
+	function OnDrawGizmos () {
+		if ( map == null ) { return; }
+		if ( map.nodes == null ) { return; }
+		
+		if ( bounds != null ) {
+			Gizmos.color = Color.white;
+		
+			Gizmos.DrawWireCube ( bounds.center, bounds.size );
+		}
+		
+		for ( var n : OPNode in map.nodes ) {		
+			if ( n == null ) { continue; }
+			
+			Gizmos.color = new Color ( 1, 1, 1, 0.5 );
+			
+			if ( n.parent ) { Gizmos.color = Color.red; }
+			if ( n.active ) { Gizmos.color = Color.green; }
+			if ( n.neighbors.Count < 1 ) { Gizmos.color = Color.red; }
+			
+			Gizmos.DrawCube ( n.position, new Vector3 ( 0.1, 0.1, 0.1 ) );
+			
+			for ( var nb : OPNode in n.neighbors ) {
+				if ( n.active && nb.active ) { Gizmos.color = Color.green; }
+				else { Gizmos.color = Color.white; }
+				
+				Gizmos.DrawLine ( n.position, nb.position );
+			}
+			
+			Gizmos.color = Color.white;
+		}
+	}
+	
+	function GetClosestNode ( pos : Vector3 ) : OPNode {
+		var shortestDistance : float = 100;
+		var node : OPNode;
+		
+		for ( var n : OPNode in map.nodes ) {
+			if ( n == null ) { continue; }
+			
+			var currentDistance : float = ( pos - (n as OPNode).position ).magnitude;
+			
+			if ( currentDistance < shortestDistance && ( n as OPNode ).neighbors.Count > 0 ) {
+				shortestDistance = currentDistance;
+				node = n as OPNode;
+				
+			}
+		}
+		
+		return node;
+	}
+}
